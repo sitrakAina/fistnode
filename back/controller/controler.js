186b0092733db6898baa484617f1b68fb648db87
leesimplon <sitrakaniaina812@gmail.com>
2019-04-15 15:25:58 +0300

    crund node

---
+++ back/controller/controler.js
@@ -0,0 +1,101 @@
+const fs = require('fs');
+const express = require('express')
+const router = express.Router()
+
+exports.getNote = (req, res) => {
+    try{
+        let read = fs.readFileSync('./model/note.json', 'utf8')
+        let parserImage = JSON.parse(read)
+         for (let i = 0; i < parserImage.length; i++) {
+ 
+              router.get(function (req, res) {
+                   try {
+                        res.end()
+ 
+                   } catch (e) {
+                        console.log(e.stack);
+                   }
+              
+              })
+         }
+         res.end(read)
+    }catch(e){
+         console.log(e.stack);
+    }
+ };
+
+exports.getTest = (req, res) => {
+     try{
+         let read = fs.readFileSync('./model/test.json', 'utf8')
+         let parserImage = JSON.parse(read)
+         for (let i = 0; i < parserImage.length; i++) {
+ 
+              router.get(function (req, res) {
+                   try {
+                        res.end()
+ 
+                   } catch (e) {
+                        console.log(e.stack);
+                   }
+              
+              })
+         }
+         res.end(read)
+    }catch(e){
+         console.log(e.stack);
+    }
+ };
+
+ exports.postList = (req, res) => {
+     let nom = req.body.nom;
+     let prenom = req.body.prenom;
+         let entre = fs.readFileSync('./model/note.json', 'utf8');
+         let com = JSON.parse(entre);
+         if (com.length > 0) {
+              var id = com[com.length - 1].id + 1;
+         }else{
+              id = 0;
+         }
+         com.push({id:id, nom:nom, prenom:prenom});
+
+    fs.writeFileSync('./model/note.json', JSON.stringify(com));
+    res.send(com);
+ };
+ 
+ exports.putList = (req, res) => {
+     let id = parseInt(req.body.id);
+     let nom = req.body.nom;
+     let prenom = req.body.prenom;
+ 
+     let entre = fs.readFileSync('./model/note.json', 'utf-8')
+      let com = JSON.parse(entre)
+ 
+      for (let i = 0; i < com.length; i++) {
+           if (id === i) {
+                if (nom) {
+                    com[i].nom = nom
+                }
+                if (prenom) {
+                    com[i].prenom = prenom
+                }
+           }
+ 
+      }
+      
+      fs.writeFileSync('./model/note.json', JSON.stringify(com));
+      res.send(com);
+ }; 
+ 
+ exports.deleteList = (req, res) => {
+     let id = parseInt(req.body.id)
+      let entre = fs.readFileSync('./model/note.json', 'utf-8')
+      let com = JSON.parse(entre)
+      for (let i = 0; i< com.length; i++){
+           if(id == com[i].id){
+               com.splice(i,1)
+           } 
+      }
+      res.send(JSON.stringify(com))
+      fs.writeFileSync('./model/note.json', JSON.stringify(com))
+ };
+
--- back/route/route.js
+++ back/route/route.js
@@ -1,45 +1,14 @@
 const express = require ('express')
+const router = express.Router();
+const controler = require('../controller/controler');
 
-const router = express.Router()
-const fs   = require('fs');
+router.route('/list')
+     .get(controler.getNote)
+     .post(controler.postList)
+     .put(controler.putList)
+     .delete(controler.deleteList);
 
+router.route('/test').get(controler.getTest);
 
-router.get('/', function (req, res) {
-res.send('hello world')
-})
-
-router.get('/list', function (req, res) {
-     res.setHeader('Content-Type', 'text/plain');
-     try{
-          let read = fs.readFileSync('./note.json', 'utf8')
-          let parserImage = JSON.parse(read)
-          for (let i = 0; i < parserImage.length; i++) {
-
-               router.get(parserImage[i].image, function (req, res) {
-                    try {
-                         var sary = '.' + parserImage[i].image
-                         var liresary = fs.readFileSync(sary)
-                         console.log(liresary)
-                         res.write(liresary)
-                         res.end()
-
-                    } catch (e) {
-                         console.log(e.stack);
-                    }
-               
-               })
-          }
-          res.end(read)
-     }catch(e){
-          console.log(e.stack);
-     }
-     
-})
-router.get('/test', function (req, res) {
-     let read = fs.readFileSync('./test.json', 'utf8')
-      res.write(read)
-      res.end()
- 
-})
 
 module.exports = router;
